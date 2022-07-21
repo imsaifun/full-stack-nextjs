06a2258ce68d4d9af164c4afdf4bd065935f4bdc
imsaifun <imsaifun@gmail.com>
2022-07-21 18:50:13 +0600

    working

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -21,9 +21,7 @@ const Index = ({ user, orders, products }) => {
         }
     };
 
-    // console.log(orderList);
-    // const item = orderList.filter((order) => order._id === id)[0];
-
+    
     const handleStatus = async (id) => {
         const item = orderList.filter((order) => order._id === id)[0];
         // console.log(item);
--- pages/orders/[id].js
+++ pages/orders/[id].js
@@ -4,16 +4,18 @@ import getOrderById from "../../lib/getOrderById";
 
 export default function OrderDetails({ order }) {
     const status = order.status
-    console.log(status);
+    // console.log(status);
+
+
 
     return (
         <Layout pageClass="admin" role="admin">
-            <h1>Order Details</h1>
 
             <div className="row">
                 <div className="col-12">
+                    <h4 className="card-title mb-30">Order Status</h4>
                     <div className="card">
-                        <div className="card-body">
+                        <div className="card-body  py-50">
                             <ul class="tab-steps--list">
                                 <li class={status <= 4 && status >= 0 && "active"}>Ordered</li>
                                 <li class={status <= 4 && status >= 1 && "active"}>Preparing</li>
@@ -21,70 +23,31 @@ export default function OrderDetails({ order }) {
                                 <li class={status <= 4 && status >= 3 && "active"}>Delivered</li>
                                 <li class={status <= 4 && status >= 4 && "active"}>Completed</li>
                             </ul>
+
+                            <div className="row justify-content-center text-center">
+                                <div className="col-xl-3">
+                                    <span>Buyer Name</span>
+                                    <h4>{order.customer}</h4>
+                                </div>
+                                <div className="col-xl-3">
+                                    <span>Delivery Address</span>
+                                    <h4>{order.address}</h4>
+                                </div>
+                                <div className="col-xl-3">
+                                    <span>Order Price</span>
+                                    <h4>{order.total}</h4>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <p>
-                <strong>Name</strong>: {order.customer}
-                <br />
-                <strong>Address</strong>: {order.address}
-                <br />
-                <strong>Price</strong>: {order.total}
 
-            </p>
-
-            <br />
-            <br />
-            <br />
-            <br />
-
-            {/* <ul class="progressbar">
-        <li class={status <= 4 && status >= 0 && "active"}>Ordered</li>
-        <li class={status <= 4 && status >= 1 &&  "active"}>Preparing</li>
-        <li class={status <= 4 && status >= 2 &&  "active"}>On the way</li>
-        <li class={status <= 4 && status >= 3 &&  "active"}>Delivered</li>
-        <li class={status <= 4 && status >= 4 &&  "active"}>Completed</li>
-      </ul> */}
-
-
-
-            {/* <ol class="steps">
-        <li class={status <= 4 && status >= 0 && "step is-complete"} data-step="1">
-          Ordered
-        </li>
-        <li class="step is-active" data-step="2">
-          Preparing
-        </li>
-        <li class="step" data-step="3">
-          On the way
-        </li>
-        <li class="step" data-step="4">
-          Delivered
-        </li>
-        <li class="step" data-step="4">
-          Completed
-        </li>
-      </ol> */}
-
-            {/* <button onClick={signoutHandler}>Sign out</button> */}
         </Layout>
     );
 }
 
-// export async function getServerSideProps({ params }) {
-//     await dbConnect()
-
-//     const product = await Product.findById(params.id).lean()
-//     product._id = product._id.toString()
-
-//     const allProducts = JSON.stringify(product)
-//     const pizza = JSON.parse(allProducts)
-
-//     return { props: { pizza } }
-
-// }
 
 
 export async function getServerSideProps({ req, res, params }) {
